add3a825cadebbbc25f137ddd0d73c23c10056c7
Sebastian Green <sgre@netlight.com>
2024-01-23 16:36:09 +0100

    Compose building docker image

--- src/Habits.gRPC/Program.cs
+++ src/Habits.gRPC/Program.cs
@@ -14,7 +14,11 @@ var app = builder.Build();
 app.MapGrpcService<HabitsService>();
 app.MapGrpcReflectionService();
 app.MapGet("/",
-    () =>
-        "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+    (ILogger<Program> logger) =>
+    {
+        logger.LogInformation("In GET /");
+        return
+            "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909";
+    });
 
 app.Run();
